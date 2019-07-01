1ccd93826eb108acd08dd07176a114a9e66b3b24
JillJohnson <jillj333@gmail.com>
2019-07-01 14:55:36 -0500

    Add files via upload

---
+++ DatabaseInterfaceTools/src/AddRecord.java
@@ -0,0 +1,31 @@
+import java.sql.*
+
+public class DBGeneral {
+	
+	// Database interface for new clients.  Will eventually call google maps API to map the 
+	// location of the client. 
+
+	String NameLast;
+	String NameFirst;
+	String AddressLine1;
+	String AddressLine2;
+	String City;
+	String State;
+	String Zip;
+	
+	Public DBGeneral (String newNameLast, String newNameFirst, String newCity) 
+	{
+		
+		
+	}
+	
+	Public AddNewRecord (String NewNameFirst, 
+							  String NewNameLast, 
+							  String AddressLine1, 
+							  String AddressLine2, 
+							  String City  ) 
+	{
+		
+	}
+
+}
